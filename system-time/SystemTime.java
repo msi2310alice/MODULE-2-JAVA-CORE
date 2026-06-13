d16835af781b754358d7285907208e32142146ce
Kieu My <msi2310alice@gmail.com>
2026-06-13 11:16:38 +0700

    realtime

---
+++ system-time/SystemTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) throws Exception {
+        Date now = new Date(); //Lấy thời gian hiện tại 
+        System.out.println("Now is: " + now);
+    }
+}
