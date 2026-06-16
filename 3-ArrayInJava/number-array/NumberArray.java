398572eabaa60ebdf7624c153d0df2e440e3de73
Kieu_My <msi2310alice@gmail.com>
2026-06-16 21:10:36 +0700

    max

--- 3-ArrayInJava/number-array/NumberArray.java
+++ 3-ArrayInJava/number-array/NumberArray.java
@@ -4,6 +4,7 @@ public class NumberArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size;
+        int[] array;
         do {
             System.out.print("Enter a size: ");
             size = scanner.nextInt();
@@ -11,6 +12,27 @@ public class NumberArray {
                 System.out.println("Size should not exceed 20.");
             } 
         } while (size>20);
+        array = new int[size];
+        int i = 0;
+        while (i<array.length){
+            System.out.print("Enter element"+(i+1)+" : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.print("Property list: ");
+        for (int j=0; j<array.length; j++){
+            System.out.print(array[j]+"\t");
+        }
+        int max = array[0];
+        int index = 1;
+        for (int j=0; j<array.length; j++){
+            if (max<array[j]){
+                max = array[j];
+                index = j+1;
+            }
+        }
+        System.out.print("The largest property value in the list is "+max+", at position "+ index);
+
     }
     
 }
